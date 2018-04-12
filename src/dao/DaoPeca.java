28591401dcd47a9fd4cd7a9bbabb7dcb7952882c
LuizV1 <LuizV1@192.168.201.130>
2018-04-12 00:00:50 -0300

    Cadastro de dados no banco por tela funcionando. Pendente polimento.

--- src/dao/DaoPeca.java
+++ src/dao/DaoPeca.java
@@ -5,10 +5,72 @@
  */
 package dao;
 
+import java.sql.*;
+
 /**
  *
  * @author LuizV1
  */
 public class DaoPeca {
+    private int codigoPeca;
+    private int categoriaPeca;
+    private String nomePeca;
+    private int quantidadePeca;
+    private float valorUnitario;
     
+    public void cadastrarPeca(int cod, String nome, String cat, int qtd, float valor) throws SQLException, ClassNotFoundException {
+        try{
+        Connection con = Conexao.conectar();
+        Statement st = con.createStatement();
+        st.executeQuery("INSERT INTO SYNCHROSOFT.TB_PECA VALUES ("+cod+",'"+nome+"','"+cat+"',"+qtd+","+valor+")");
+        st.close();
+        con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+
+    public int getCodigoPeca() {
+        return codigoPeca;
+    }
+
+    public void setCodigoPeca(int codigoPeca) throws SQLException, ClassNotFoundException {
+        this.codigoPeca = codigoPeca;
+        
+    }
+
+    public int getCategoriaPeca() {
+        return categoriaPeca;
+    }
+
+    public void setCategoriaPeca(int categoriaPeca) {
+        this.categoriaPeca = categoriaPeca;
+    }
+
+    public String getNomePeca() {
+        return nomePeca;
+    }
+
+    public void setNomePeca(String nomePeca) {
+        this.nomePeca = nomePeca;
+    }
+
+    public int getQuantidadePeca() {
+        return quantidadePeca;
+    }
+
+    public void setQuantidadePeca(int quantidadePeca) {
+        this.quantidadePeca = quantidadePeca;
+    }
+
+    public float getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(float valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+   
 }
