f5312509430a1edc63954aeddbdefc4627eeb1c3
Administrator <Administrator@10.0.2.15>
2018-04-18 19:27:48 -0300

    Classe MAIN SynchroSoft movida para pacote Control. Pacote Synchrosoft removido. Commit feito na VM XP64.

--- src/control/SynchroSoft.java
+++ src/control/SynchroSoft.java
@@ -3,7 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package synchrosoft;
+package control;
+
 
 import java.sql.*;
 import javax.swing.JFrame;
